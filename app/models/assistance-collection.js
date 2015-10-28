ab9b3b5545454e2aa43601aadee297b98da85d54
Richard <rfree86@gmail.com>
2015-10-27 22:41:46 -0400

    completed asstcollection but not tested.

---
+++ app/models/assistance-collection.js
@@ -0,0 +1,26 @@
+import Assistance from './assistance';
+
+var AssistanceCollection = Backbone.Collections.extend({
+  model: Assistance,
+  url() {
+    //should it be caseId or clientId?
+    //am I trying to target the User, or the client/case?
+    return "https://api.parse.com/1/classes/asst" + JSON.stringify({
+      case: {
+        __type: "Pointer",
+        className: "Recipe",
+        objectId: this.caseId
+      }
+    });
+  },
+
+  initialize(options) {
+    this.caseId = options.caseId;
+  },
+
+  parse(response) {
+    return response.results;
+  }
+});
+
+export default AssistanceCollection;
